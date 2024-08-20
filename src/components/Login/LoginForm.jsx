51948e20c70600d05172b7f461685f6f207ff53c
Coder-Saiful <md.syful.max970@gmail.com>
2024-08-20 19:59:50 +0600

    remove next.js middleware

--- src/components/Login/LoginForm.jsx
+++ src/components/Login/LoginForm.jsx
@@ -2,7 +2,7 @@
 import Link from "next/link";
 import React, { useState } from "react";
 import { toast } from "react-toastify";
-import { useRouter, useSearchParams } from 'next/navigation';
+import { useRouter, useSearchParams } from "next/navigation";
 import { httpAxios } from "@/helper/httpAxios";
 
 const LoginForm = () => {
@@ -35,26 +35,24 @@ const LoginForm = () => {
           password: "",
         });
         toast.success(response.data.message);
-        
-        setTimeout(() => {
-          if (redirectUrl) {
-              router.push(`${redirectUrl}`);
-          } else {
-            switch(response.data.user.role) {
-              case 'user':
-                router.push(`/accounts/profile`);
-                break;
-              case 'manager':
-                router.push('/admin/dashboard');
-                break;
-              case 'admin':
-                router.push('/admin/dashboard');
-                break;
-              default:
-                router.push('/');
-            } 
+
+        if (redirectUrl) {
+          router.push(`${redirectUrl}`);
+        } else {
+          switch (response.data.user.role) {
+            case "user":
+              router.push(`/accounts/profile`);
+              break;
+            case "manager":
+              router.push("/admin/dashboard");
+              break;
+            case "admin":
+              router.push("/admin/dashboard");
+              break;
+            default:
+              router.push("/");
           }
-        }, 1000);
+        }
       })
       .catch((error) => {
         setIsLoading(false);
@@ -67,7 +65,7 @@ const LoginForm = () => {
           if (error.response.data.invalid_credential) {
             setErrors((preValue) => ({
               ...preValue,
-              password: error.response.data.invalid_credential
+              password: error.response.data.invalid_credential,
             }));
           }
         } else {
@@ -78,46 +76,44 @@ const LoginForm = () => {
   };
 
   return (
-      <form onSubmit={handleSubmit}>
-        <div className="mb-3">
-          <label className="form-label">Enter your email address</label>
-          <input
-            type="text"
-            className={`form-control ${errors.email ? "is-invalid" : ""}`}
-            name="email"
-            value={userdata.email}
-            onChange={handleChange}
-          />
-          {errors.email && (
-            <div className="invalid-feedback">{errors.email}</div>
-          )}
-        </div>
-        <div className="mb-1">
-          <label className="form-label">Enter your password</label>
-          <input
-            type="password"
-            className={`form-control ${errors.password ? "is-invalid" : ""}`}
-            name="password"
-            value={userdata.password}
-            onChange={handleChange}
-          />
-          {errors.password && (
-            <div className="invalid-feedback">{errors.password}</div>
-          )}
-          </div>
-        <div className="text-end mb-3">
-          <Link
-            href={`/accounts/forgot-password`}
-            className="border-0 bg-transparent forgot_pass_link"
-            style={{ color: "var(--primaryColor)", fontSize: "14px" }}
-          >
-            Forgot Password
-          </Link>
-        </div>
-        <button type="submit" className="btn btn-dark w-100" disabled={isLoading}>
-          {isLoading ? "Submitting..." : "Submit"}
-        </button>
-      </form>
+    <form onSubmit={handleSubmit}>
+      <div className="mb-3">
+        <label className="form-label">Enter your email address</label>
+        <input
+          type="text"
+          className={`form-control ${errors.email ? "is-invalid" : ""}`}
+          name="email"
+          value={userdata.email}
+          onChange={handleChange}
+        />
+        {errors.email && <div className="invalid-feedback">{errors.email}</div>}
+      </div>
+      <div className="mb-1">
+        <label className="form-label">Enter your password</label>
+        <input
+          type="password"
+          className={`form-control ${errors.password ? "is-invalid" : ""}`}
+          name="password"
+          value={userdata.password}
+          onChange={handleChange}
+        />
+        {errors.password && (
+          <div className="invalid-feedback">{errors.password}</div>
+        )}
+      </div>
+      <div className="text-end mb-3">
+        <Link
+          href={`/accounts/forgot-password`}
+          className="border-0 bg-transparent forgot_pass_link"
+          style={{ color: "var(--primaryColor)", fontSize: "14px" }}
+        >
+          Forgot Password
+        </Link>
+      </div>
+      <button type="submit" className="btn btn-dark w-100" disabled={isLoading}>
+        {isLoading ? "Submitting..." : "Submit"}
+      </button>
+    </form>
   );
 };
 
--- src/middleware.js
+++ src/middleware.js
@@ -7,51 +7,51 @@ export async function middleware(request) {
   const publicUrl = ["/accounts/login", "/accounts/register"];
   const currentPathname = request.nextUrl.pathname;
 
-  if (token) {
-    try {
-      const { payload } = await jwtVerify(
-        token,
-        new TextEncoder().encode(process.env.JWT_SECRET)
-      );
+  // if (token) {
+  //   try {
+  //     const { payload } = await jwtVerify(
+  //       token,
+  //       new TextEncoder().encode(process.env.JWT_SECRET)
+  //     );
 
-      if (publicUrl.includes(currentPathname)) {
-        if (payload.role == "user") {
-          return NextResponse.redirect(
-            new URL("/accounts/profile", request.url)
-          );
-        }
+  //     if (publicUrl.includes(currentPathname)) {
+  //       if (payload.role == "user") {
+  //         return NextResponse.redirect(
+  //           new URL("/accounts/profile", request.url)
+  //         );
+  //       }
 
-        if (payload.role == "manager" || payload.role == "admin") {
-          return NextResponse.redirect(
-            new URL("/admin/dashboard", request.url)
-          );
-        }
-      }
+  //       if (payload.role == "manager" || payload.role == "admin") {
+  //         return NextResponse.redirect(
+  //           new URL("/admin/dashboard", request.url)
+  //         );
+  //       }
+  //     }
 
-      if (currentPathname.startsWith('/admin') && payload.role == "user" ) {
-        return NextResponse.redirect(
-          new URL("/accounts/profile", request.url)
-        );
-      }
+  //     if (currentPathname.startsWith('/admin') && payload.role == "user" ) {
+  //       return NextResponse.redirect(
+  //         new URL("/accounts/profile", request.url)
+  //       );
+  //     }
 
-    } catch (error) {
-      console.log(
-        error.name == "JWSInvalid" ? "Invalid token" : "Token decoding error."
-      );
-      return NextResponse.redirect(new URL(`/accounts/login`, request.url));
-    }
-  } else {
-    if (publicUrl.includes(currentPathname)) {
-      return NextResponse.next();
-    }
-    if (
-      currentPathname.startsWith("/admin") ||
-      currentPathname.startsWith("/accounts") ||
-      currentPathname.startsWith("/tasks")
-    ) {
-      return NextResponse.redirect(new URL(`/accounts/login`, request.url));
-    }
-  }
+  //   } catch (error) {
+  //     console.log(
+  //       error.name == "JWSInvalid" ? "Invalid token" : "Token decoding error."
+  //     );
+  //     return NextResponse.redirect(new URL(`/accounts/login`, request.url));
+  //   }
+  // } else {
+  //   if (publicUrl.includes(currentPathname)) {
+  //     return NextResponse.next();
+  //   }
+  //   if (
+  //     currentPathname.startsWith("/admin") ||
+  //     currentPathname.startsWith("/accounts") ||
+  //     currentPathname.startsWith("/tasks")
+  //   ) {
+  //     return NextResponse.redirect(new URL(`/accounts/login`, request.url));
+  //   }
+  // }
 
   return NextResponse.next();
 }
