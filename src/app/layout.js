9205145e3b1830f67af855af1e6b235b967260e2
Coder-Saiful <md.syful.max970@gmail.com>
2024-09-02 06:52:10 +0600

    change form background color

--- src/app/layout.js
+++ src/app/layout.js
@@ -27,7 +27,7 @@ export default function RootLayout({ children }) {
         <head>
           {/* <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"/> */}
         </head>
-        <body className={`${poppins.className} bg-dark`}>
+        <body className={`${poppins.className}`}>
           <AuthProvider>
           <Header />
           <main>
