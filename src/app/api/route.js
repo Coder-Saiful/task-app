99a643dcd214c0626a5880c2fd94fbfb8cec3a38
Coder-Saiful <md.syful.max970@gmail.com>
2024-09-02 06:44:09 +0600

    category, subcategory and nastedsubcategory crud api have been created

--- src/app/api/route.js
+++ src/app/api/route.js
@@ -3,6 +3,6 @@ import { Category } from "@/models/category";
 
 export async function GET() {
 //   const result = await Category.create();
-  console.log(result);
+  // console.log(result);
   return SendResponse("the query successfully run");
 }
